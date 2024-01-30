7b999bdf8c518599c68e9b83b78c35bd8e364e41
Alba <albadvg@yahoo.es>
2024-01-30 20:43:50 +0100

    first main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,30 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
 import './Home.scss';
-import SimpleBar from "simplebar-react";
+// import SimpleBar from "simplebar-react";
 import 'simplebar-react/dist/simplebar.min.css';
 
 const Home = () => {
-    const { t, i18n } = useTranslation()
-    const changeLanguage = () => {
-        i18n.changeLanguage("en")
-    }
+    const { t } = useTranslation()
+    // i18n 
+    // const changeLanguage = () => {
+    //     i18n.changeLanguage("en")
+    // }
 
-    const changeLanguageTo = () => {
-        i18n.changeLanguage("es")
-    }
+    // const changeLanguageTo = () => {
+    //     i18n.changeLanguage("es")
+    // }
 
     return (
-        <>
-            <SimpleBar style={{ maxHeight: 300 }}>
+        <div className="home">
+            {/* <SimpleBar style={{ maxHeight: 300 }}>
                 <h1>{t("titulo")}</h1>
                 <button onClick={changeLanguage}>cambiar</button>
                 <button onClick={changeLanguageTo}>cambiar</button>
 
-            </SimpleBar>
-        </>
+            </SimpleBar> */}
+            <h1 className="home__title">{t("titulo")}</h1>
+        </div>
     )
 }
 
