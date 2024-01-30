e21ecffd3c9c78fac03bbd4b72bfbecc2215997c
david-patino <davidpatmal13@gmail.com>
2024-01-30 21:16:23 +0100

    navBar-completed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import { useTranslation } from "react-i18next"
+import "./Nav.scss"
+import { Link } from "react-router-dom"
 
 const Nav = () => {
+    const { t } = useTranslation()
+
     return (
-        <div>
-            <h1>Hola soy el nav</h1>
+        <div className="nav__container">
+            <Link to="/characters">
+                <h2>{t("personajes")}</h2>
+            </Link>
+            <Link to="/houses">
+                <h2>{t("casas")}</h2>
+            </Link>
+            <Link to="/timeline">
+                <h2>{t("cronologia")}</h2>
+            </Link>
         </div>
     )
 }
